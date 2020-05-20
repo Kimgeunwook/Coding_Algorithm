9afd1db6080af658292f3845b2d2962f364ab3a0
wook9 <wook961206@naver.com>
2020-05-20 17:44:13 +0900

    사다리조작 pypy버전

--- 프로그래머스/사다리조작.py
+++ 프로그래머스/사다리조작.py
@@ -1,44 +1,47 @@
 from itertools import combinations
 import copy
-def search(temp):
+def search():
     for j in range(1,N+1):
         cur_location = j
         for i in range(1, H + 1):
             if cur_location == 1:
-                if temp[i][cur_location] == 1:
+                if _map[i][cur_location] == 1:
                     cur_location += 1
             elif cur_location == N:
-                if temp[i][cur_location - 1] == 1:
+                if _map[i][cur_location - 1] == 1:
                     cur_location -= 1
             else:
-                if temp[i][cur_location] == 1 :
+                if _map[i][cur_location] == 1 :
                     cur_location += 1
-                elif temp[i][cur_location - 1] == 1:
+                elif _map[i][cur_location - 1] == 1:
                     cur_location -= 1
         if cur_location != j:
             return -1
     return 1
 
 def bfs():
-    if search(_map) == 1:
+    if search() == 1:
         return 0
 
     for i in range(1,4):
-        comb = list(combinations(comb_list,i))
-        for j in comb:
-            temp = copy.deepcopy(_map)
+        for j in combinations(comb_list,i):
+            #temp = copy.deepcopy(_map)
+            a = []
             for k in j:
-                if _map[k[0]][k[1]] == 1:
+                if _map[k[0]][k[1]] == 1 or _map[k[0]][k[1] - 1] or _map[k[0]][k[1] + 1]:
                     continue
-                temp[k[0]][k[1]] = 1
-            if search(temp) == 1:
+                _map[k[0]][k[1]] = 1
+                a.append((k[0],k[1]))
+            if search() == 1:
                 return i
+            for m in a:
+                _map[m[0]][m[1]] = 0
     return -1
 
 
 
 N, M, H = list(map(int,input().split()))
-_map = [[0]*N for _ in range(H + 1)]
+_map = [[0]*(N + 2) for _ in range(H + 2)]
 
 comb_list = []
 for i in range(1, H + 1):
