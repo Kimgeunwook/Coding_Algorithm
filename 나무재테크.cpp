2bd80e9ba63500a73b82c7b7973c17d9270eb4e8
wook9 <wook961206@naver.com>
2020-05-27 15:32:31 +0900

    나무재테크 시간 고쳤지만 그래도 초과버전

--- 나무재테크.cpp
+++ 나무재테크.cpp
@@ -3,64 +3,73 @@
 #include <vector>
 #include <functional>
 #include <queue>
+#include <time.h>
 using namespace std;
 int N, M, K;
 int map[10][10], vitamin[10][10]; // 주맵   입력양분맵
 vector<pair<int, int>> five;
-priority_queue<int, vector<int>, greater<int>> pq_tree[10][10];
+
+struct tree
+{
+	int x;
+	int y;
+	int age;
+};
+struct compare {
+	bool operator()(tree a, tree b)
+	{
+		return a.age > b.age;
+	}
+};
+
+priority_queue<tree, vector<tree>, compare> pqtree;
+priority_queue<tree, vector<tree>, compare> temp;
+int visit[10][10];
 void spring_summer()
 {
-	five.clear();
-	for(int i = 0; i < N; i++)
+	for (int i = 0; i < N; i++)
 		for (int j = 0; j < N; j++)
+			visit[i][j] = 0;
+	five.clear();
+	while (!pqtree.empty())
+	{
+		int cur_x = pqtree.top().x;
+		int cur_y = pqtree.top().y;
+		int cur_age = pqtree.top().age;
+		if (cur_age <= map[cur_x][cur_y] && visit[cur_x][cur_y] == 0) // 양분 먹을수있으면
 		{
-			int sum = 0;
-			priority_queue<int, vector<int>, greater<int>> temp[10][10];
-			int size = pq_tree[i][j].size();
-			
-			int original = map[i][j];
-			for (int k = 0; k < size; k++)
-			{
-	
-				if (sum + pq_tree[i][j].top() <= original)//양분 먹을수 있으면
-				{
-					sum += pq_tree[i][j].top();
-					map[i][j] -= pq_tree[i][j].top();
-					temp[i][j].push(pq_tree[i][j].top() + 1);
-					if ((pq_tree[i][j].top() + 1) % 5 == 0)
-					{
-						five.push_back(make_pair(i, j));
-					}
-					pq_tree[i][j].pop();
-				}
-				else
-				{
-					if (pq_tree[i][j].size() != 0)
-					{
-						map[i][j] += pq_tree[i][j].top() / 2;
-						pq_tree[i][j].pop();
-					}
-				}
-				
-			}
-			pq_tree[i][j] = temp[i][j];
+			map[cur_x][cur_y] -= cur_age;
+			cur_age += 1;
+			if (cur_age % 5 == 0)
+				five.push_back(make_pair(cur_x,cur_y));
+			tree next = { cur_x,cur_y,cur_age };
+			temp.push(next);
 		}
-
-	
+		else // 양분 못먹으면
+		{
+			visit[cur_x][cur_y] = 1;
+			map[cur_x][cur_y] += (cur_age / 2);
+		}
+		pqtree.pop();
+	}
+	pqtree = temp;
+	while (!temp.empty())
+		temp.pop();
 }
 void fall()
 {
-	for (int i = 0; i < five.size(); i++)
-	{	
+	int size = five.size();
+	for (int i = 0; i < size; i++)
+	{
 		int cur_x = five[i].first;
 		int cur_y = five[i].second;
-		for (int i = -1; i < 2; i++)
-			for (int j = -1; j < 2; j++)
-			{
-				if (!(i == 0 && j == 0))
-					if (cur_x + i >= 0 && cur_x + i < N && cur_y + j >= 0 && cur_y + j < N)
-						pq_tree[cur_x + i][cur_y + j].push(1);
-			}	
+		for(int a = -1; a < 2; a++)
+			for(int b = -1; b < 2; b++)
+				if (!(a == 0 && b == 0) && cur_x + a >= 0 && cur_x + a < N && cur_y + b >= 0 && cur_y + b < N)
+				{
+					tree add = {cur_x + a, cur_y + b,1};
+					pqtree.push(add);
+				}
 	}
 }
 void winter()
@@ -69,23 +78,18 @@ void winter()
 		for (int j = 0; j < N; j++)
 			map[i][j] += vitamin[i][j];
 }
-int answer()
-{
-	int ret = 0;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			ret += pq_tree[i][j].size();
-	return ret;
-}
 int main()
 {
+	time_t start, end;
+	double result; 
+	start = time(NULL);
 	cin >> N >> M >> K; //맵크기   나무개수   몇년지날건지
 
 	for (int i = 0; i < N; i++) //처음 나무 양분5로 초기화
 		for (int j = 0; j < N; j++)
 			map[i][j] = 5;
 
-	for(int i = 0; i < N; i++)
+	for (int i = 0; i < N; i++) //겨울양분
 		for (int j = 0; j < N; j++)
 			cin >> vitamin[i][j];
 
@@ -93,15 +97,20 @@ int main()
 	{
 		int x, y, age;
 		cin >> x >> y >> age;
-		pq_tree[x - 1][y - 1].push(age);
+		tree a = { x - 1,y - 1,age };
+		pqtree.push(a);
 	}
-		
+
+	
+
 	for (int i = 0; i < K; i++)////////K번반복
 	{
 		spring_summer();
 		fall();
 		winter();
 	}
-	cout << answer() << endl;
+	cout << pqtree.size() << endl;
+	end = time(NULL);
+	cout << double(end - start) << endl;
 	return 0;
 }
