80e1543fdb78e7bb88debae0806d5fb5bfb88b6d
wook9 <wook961206@naver.com>
2020-09-04 14:50:43 +0900

    팰린드롬수정

--- 그리디/프로그래머스가장긴팰린드롬.cpp
+++ 그리디/프로그래머스가장긴팰린드롬.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
 using namespace std;
-int main()
+int solution(string s)
 {
-	string s = "abcdefg";
-	
-	cout << s.substr(3) << endl; //defg
-	cout << s.substr(3, 2) << endl; //de
-	cout << s.substr(3, 200) << endl; //defg
-	cout << s.substr(100) << endl;
-	return 0;
+	int answer = 1;
+	for (int i = 0; i < s.size(); i++)
+	{
+		int temp = 0;
+		for (int j = 1; j < s.size(); j++)
+		{
+			if (i - j < 0 || i + j >= s.size()) break;
+			if (s[i - j] == s[i + j]) temp++;
+			else break;
+		}
+		answer = max(answer, 2 * temp + 1);
+	}
+
+	for (int i = 0; i < s.size() - 1; i++)
+	{
+		int temp = 0;
+		if (s[i] == s[i + 1])
+		{
+			for (int j = 1; j < s.size(); j++)
+			{
+				if (i - j < 0 || i + 1 + j >= s.size()) break;
+				if (s[i - j] == s[i + 1 + j]) temp++;
+				else break;
+			}
+			answer = max(answer, 2 * (temp + 1));
+		}
+
+	}
+
+
+	return answer;
 }
