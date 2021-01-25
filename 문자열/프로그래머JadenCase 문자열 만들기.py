7dd32075bd3cf4ea2dcc91922dd12f569813bed4
wook9 <wook961206@naver.com>
2021-01-25 22:24:20 +0900

    기본

--- 문자열/프로그래머JadenCase 문자열 만들기.py
+++ 문자열/프로그래머JadenCase 문자열 만들기.py
@@ -1,16 +1,22 @@
-def solution(s):
-    answer = ''
+# def solution(s):
+#     answer = ''
+#
+#     flag = True
+#     for idx in range(len(s)):
+#         if s[idx] == ' ':
+#             flag = True
+#             answer = answer + ' '
+#         elif flag:
+#             flag = False
+#             answer = answer + s[idx].upper()
+#         else:
+#             flag = False
+#             answer = answer + s[idx].lower()
+#
+#     return answer
 
-    flag = True
-    for idx in range(len(s)):
-        if s[idx] == ' ':
-            flag = True
-            answer = answer + ' '
-        elif flag:
-            flag = False
-            answer = answer + s[idx].upper()
-        else:
-            flag = False
-            answer = answer + s[idx].lower()
-
-    return answer
+a = []
+a.append('a')
+a.push('b')
+a.push('c')
+print(a)
